2f568d0ed2e07574f65f094fe29ab644bcfbb546
subaguruprabakaran <subaguruprabagaran@gtcollege.in>
2025-06-20 13:01:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# YOUTUBE CLONE
+
+A YouTube clone built with React, Tailwind CSS, Redux, and YouTube APIs.
---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+const GOOGLE_API_KEY = 'AIzaSyCwGUxQyN6g_Dzz-Z063NdL_KY84DQnplg'
+
+export const LIVE_CHAT_COUNT = 10
+
+export const autosuggest =
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const keyword_search =
+  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="
+
+export const YOUTUBE_VIDEOS_API = 
+'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyCwGUxQyN6g_Dzz-Z063NdL_KY84DQnplg';
+
+export const YOUTUBE_SEARCH_API =
+'https://thingproxy.freeboard.io/fetch/https://clients1.google.com/complete/search?client=youtube&gs_ri=youtube&ds=yt&q='
